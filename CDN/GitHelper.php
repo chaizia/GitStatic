4f285445e9343d17fbb24eedc6aed35e924c1b8b
乔千 <61873808+MliKiowa@users.noreply.github.com>
2020-04-10 17:42:40 +0800

    🔨逻辑整理
    修复bug一处

--- CDN/GitHelper.php
+++ CDN/GitHelper.php
@@ -10,84 +10,71 @@ function remote_file_exists($url) {
     } 
     return false; 
   }
-    function api_push($username,$token,$curl_url,$data,$method){
-        $curl_token_auth = 'Authorization: token ' . $token;
-        $ch = curl_init($curl_url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array( 'User-Agent: $username', $curl_token_auth ));
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-        curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
-        $response = curl_exec($ch); 
-        curl_close($ch);
-        var_dump($response);
-        return $response;
+  function api_push($username,$token,$curl_url,$data,$method){
+      $curl_token_auth = 'Authorization: token ' . $token;
+      $ch = curl_init($curl_url);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array( 'User-Agent: $username', $curl_token_auth ));
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+      curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
+      $response = curl_exec($ch); 
+      curl_close($ch);
+      return $response;
+    }
+    function api_get($url)
+      {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$url); //设置访问的url地址
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array( 'User-Agent: GitStatic'));
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);//不输出内容
+        $result = curl_exec($ch);
+        curl_close ($ch);
+        return $result;
       }
-      function api_get($url)
-        {
-          $ch = curl_init();
-          curl_setopt($ch, CURLOPT_URL,$url); //设置访问的url地址
-          curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-          curl_setopt($ch, CURLOPT_HTTPHEADER, array( 'User-Agent: GitStatic'));
-          curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-          curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);//不输出内容
-          $result = curl_exec($ch);
-          curl_close ($ch);
-          return $result;
-        }
 
-        function user_info($username) {return json_decode(api_get("https://api.github.com/users/".$username)); }
-          //不存在会返回一个message Not Found 获取用户基本信息
+      function user_info($username) {return json_decode(api_get("https://api.github.com/users/".$username)); }
+        //不存在会返回一个message Not Found 获取用户基本信息
 
-          function repos_all($username) {return json_decode(api_get("https://api.github.com/users/".$username."/repos")); }
-            //获取所有repos
+        function repos_all($username) {return json_decode(api_get("https://api.github.com/users/".$username."/repos")); }
+          //获取所有repos
 
-            function repos_info($username,$reposname) {return json_decode(api_get("https://api.github.com/repos/".$username."/".$reposname)); }
-              //获取repos info
+          function repos_info($username,$reposname) {return json_decode(api_get("https://api.github.com/repos/".$username."/".$reposname)); }
+            //获取repos info
 
-              function repos_path($username,$reposname,$path) {return json_decode(api_get("https://api.github.com/repos/".$username."/".$reposname."/contents/".$path)); }
-                //获取repos 目录内容
+            function repos_path($username,$reposname,$path) {return json_decode(api_get("https://api.github.com/repos/".$username."/".$reposname."/contents/".$path)); }
+              //获取repos 目录内容
 
-                function files_upload($username,$token,$repos,$path,$files)
+              function files_upload($username,$token,$repos,$path,$files,&$ret)
+                {
+                  $data=array("message"=>"upload by GitStatic","content"=>$files);
+                  // echo "https://api.github.com/repos/".$username."/".$repos."/contents".$path;
+                  $ret=api_push($username,$token,"https://api.github.com/repos/".$username."/".$repos."/contents".$path,json_encode($data),"PUT");
+                  $json=(array)json_decode($ret);
+                  // var_dump($json);
+                  $ret=$
+                  return !isset($json["message"]);
+                  //上传需要判断失败或者成功
+                } 
+                function files_updata($username,$token,$repos,$path,$files,$sha,&$ret)
                   {
-                    $data=array("message"=>"upload a new file","content"=>$files);
-                    // echo "https://api.github.com/repos/".$username."/".$repos."/contents".$path;
-                    $json=(array)json_decode(api_push($username,$token,"https://api.github.com/repos/".$username."/".$repos."/contents".$path,json_encode($data),"PUT"));
-                    // var_dump($json);
+                    $data=array("message"=>"updata by GitStatic","content"=>$files,"sha"=>$sha);
+                    $ret=api_push($username,$token,"https://api.github.com/repos/".$username."/".$repos."/contents".$path,json_encode($data),"PUT");
+                    $json=(array)json_decode($ret);
+                    //更新需要判断失败或者成功
+                    // var_dump($data);
                     return !isset($json["message"]);
-                    //上传需要判断失败或者成功
-                  } 
-                  function files_updata($username,$token,$repos,$path,$files,$sha)
+                  }
+                  function files_del($username,$token,$repos,$path,$sha)
                     {
-                      $data=array("message"=>"upload a new file","content"=>$files,"sha"=>$sha);
-                      $json=(array)json_decode(api_push($username,$token,"https://api.github.com/repos/".$username."/".$repos."/contents".$path,json_encode($data),"PUT"));
-                      //更新需要判断失败或者成功
-                      // var_dump($data);
+                      $data=array("message"=>"upload a new file","sha"=>$sha);
+                      $json=(array)json_decode(api_push($username,$token, "https://api.github.com/repos/".$username."/".$repos."/contents".$path,json_encode($data),"DELETE"));
+                      //var_dump($json);
                       return !isset($json["message"]);
+                      //删除需要判断失败或者成功
                     }
-                    function files_del($username,$token,$repos,$path,$sha)
-                      {
-                        $data=array("message"=>"upload a new file","sha"=>$sha);
-                        $json=(array)json_decode(api_push($username,$token, "https://api.github.com/repos/".$username."/".$repos."/contents".$path,json_encode($data),"DELETE"));
-                        //var_dump($json);
-                        return !isset($json["message"]);
-                        //删除需要判断失败或者成功
+                    function get_sha($username,$repos,$path){
+                        $json=(array)repos_path($username,$repos,$path);
+                        return $json["sha"];
                       }
-                      function get_sha($username,$repos,$path){
-                          $json=(array)repos_path($username,$repos,$path);
-                          // var_dump($json);
-                          return $json["sha"];
-                        }
-
-                        // echo files_updata("MQiaoqian","b3e7642a7fb88fe78ec9845d51688dca558cfc16","MCDN","/6484.txt","测试",get_sha("MQiaoqian","MCDN","6484.txt"));
-
-                        // files_upload("MQiaoqian","b3e7642a7fb88fe78ec9845d51688dca558cfc16","MCDN","/demo.txt","testiebdbskej");
-                        // $json=(array)repos_path("MQiaoqian","MCDN","demo.txt");
-                        // var_dump($json["sha"]);
-                        //echo get_sha("MQiaoqian","MCDN","demo.txt"); 
-                        //https://api.github.com/users/用户名/repos
-                        //api_push("MQiaoqian","b3e7642a7fb88fe78ec9845d51688dca558cfc16","/repos/solomonxie/gists/contents/2.jpg","");
-                        // var_dump(api_get("https://api.github.com/users/MQiaoqian"));
-                        // var_dump(repos_all("MQiaoqian"));
-                        function git_upload_ex()
-                          {
-                          }
--- CDN/index.php
+++ CDN/index.php
@@ -1,74 +1,101 @@
 <?php
+//https://test.9st.top/git/demo/upload/xxx.jpg?time=150000
 include dirname(__FILE__) . "/GitHelper.php";
 include dirname(__FILE__) . "/config.php";
 header("Access-control-Allow-Origin:*");
-global $real_url;
-global $path_parse;
-global $query_method;
-global $path_array;
-global $path_url;
-global $_config;
-global $extension_webp;
+$url_parse = parse_url($_SERVER['REQUEST_URI']);
+/*
+*array(2) {
+  * ["path"]=> string(24) "/git/demo/upload/xxx.jpg"
+  * ["query"]=> string(11) "time=150000"
+  * }
+*/
+$url_array = explode("/", substr($url_parse["path"], 1));
+//包括router
+/*array(4) {
+  * [0]=> string(3) "git"
+  * [1]=> string(4) "demo"
+  * [2]=> string(6) "upload" 
+  * [3]=> string(7) "xxx.jpg"
+  * }
+*/
+$real_url_p= strstr(trim($_SERVER['REQUEST_URI'], '/'), "/");
+//有参数
+//string(32) "/demo/upload/xxx.jpg?time=150000"
+
+$real_url_n = parse_url($real_url_p) ["path"];
+//无参数
+//string(20) "/demo/upload/xxx.jpg"
 $query_method = isset($GET["method"]) ? $GET["method"] : "default";
-$path_parse = parse_url($_SERVER['REQUEST_URI']);
-$path_array = explode("/", substr($path_parse["path"], 1));
-$path_url = strstr(trim($_SERVER['REQUEST_URI'], '/'), "/"); //有参数
-$real_url = parse_url($path_url) ["path"]; //无参数
-isset($config["router"][$path_array[0]]) ? : _die(10001); //非目录中断
-$_config = $config["github"][$config["router"][$path_array[0]]];
-ignore_user_abort(true);
-set_time_limit(0);
+$_config=isset($config["github"][$url_array[0]]) ?$config["github"][$url_array[0]] : _die(10001); //非目录中断
 $extension_webp = array("png" => "png", "gif" => "gif", "jpeg" => "jpeg", "jpg" => "jpeg");
-function_exists("method_" . $query_method) ? call_user_func("method_" . $query_method) : _die(10002);
-function method_default() {
-    //默认操作函数
-    $CacheFile = dirname(__FILE__) . "/hash/" . md5($GLOBALS["_Config"]["Parameter"] ? $GLOBALS["path_url"] : $GLOBALS["real_url"]) . ".md5";
-    $file_name = pathinfo($GLOBALS["real_url"]);
-    if (($GLOBALS["_config"]["CacheTime"] == 0 or time() - @filemtime($CacheFile) < $GLOBALS["_config"]["CacheTime"]) && file_exists($CacheFile)) {
-        $Cache_json = json_decode(file_get_contents($CacheFile));
-        if ($Cache_json->webp) {
-            $_path = $file_name["dirname"] == "/" ? $file_name["dirname"] . $file_name["filename"] . ".webp" : $file_name["dirname"] . "/" . $file_name["filename"] . ".webp";
-        } else {
-            $_path = $GLOBALS["path_url"];
-        }
-        //已缓存 立即导航jsdelivr
-        //如果设置跳转缓存更加高效
-        header('Location: https://cdn.jsdelivr.net/gh/' . $GLOBALS["_config"]["username"] . "/" . $GLOBALS["_config"]["repos"] . $GLOBALS["_config"]["path"] . $_path);
-        die;
-    }
-    header('Location: ' . $GLOBALS["_config"]["site"] . $GLOBALS["path_url"]);
-    fastcgi_finish_request();
-    if (isset($GLOBALS["extension_webp"][$file_name["extension"]]) && $GLOBALS["_config"]["webp"]) {
-        $Cache_json = array("webp" => true);
-        $temp_func = "imagecreatefrom" . $GLOBALS["extension_webp"][$file_name["extension"]];
-        $_path = $file_name["dirname"] == "/" ? $file_name["dirname"] . $file_name["filename"] . ".webp" : $file_name["dirname"] . "/" . $file_name["filename"] . ".webp";
-        $img_file = fopen('img/' . $file_name["basename"], "w+");
-        fwrite($img_file, file_get_contents($GLOBALS["_config"]["site"] . $GLOBALS["_config"]["path"] . $GLOBALS["path_url"]));
-        fclose($img_file);
-        $temp_img = $temp_func('img/' . $file_name["basename"]);
-        imagewebp($temp_img, 'img/' . $file_name["filename"] . ".webp");
-        imagedestroy($temp_img);
-        $file_data = file_get_contents('img/' . $file_name["filename"] . ".webp");
-        if (file_exists('img/' . $file_name["filename"] . ".webp")) @unlink('img/' . $file_name["filename"] . ".webp");
-        if (file_exists('img/' . $file_name["basename"])) @unlink('img/' . $file_name["basename"]);
-        //清理缓存
-        
-    } else {
-        $Cache_json = array("webp" => false);
-        $_path = $GLOBALS["path_url"];
-        $file_data = file_get_contents($GLOBALS["_config"]["site"] . $GLOBALS["_config"]["path"] . $GLOBALS["path_url"]);
-    }
-    $result = files_upload($GLOBALS["_config"]["username"], $GLOBALS["_config"]["token"], $GLOBALS["_config"]["repos"], $_path, base64_encode($file_data));
-    if (!$result) $result = files_updata($GLOBALS["_config"]["username"], $GLOBALS["_config"]["token"], $GLOBALS["_config"]["repos"], $_path, base64_encode($file_data), get_sha($GLOBALS["_config"]["username"], $GLOBALS["_config"]["repos"], $_path));
-    //再次出错建议写出日志
-    $Cache_json["result"] = $result;
-    file_put_contents($CacheFile, json_encode($Cache_json));
-    //写出记录
-    
-}
-function method_upload() {
-}
+//初始化webp处理后辍
 function _die($error) {
     echo "不存在";
     die;
-}
+  }
+  ignore_user_abort(true);
+  set_time_limit(0);
+  function_exists("method_" . $query_method) ? call_user_func("method_" . $query_method) : _die(10002);
+  function method_default() {
+      //默认操作函数
+      $CacheFile = dirname(__FILE__) . "/hash/" . md5($GLOBALS["_Config"]["Parameter"] ? $GLOBALS["real_url_p"] : $GLOBALS["real_url_n"]) . ".json";
+      $url_info_file = pathinfo($GLOBALS["real_url_n"]);
+      /*array(4) { 
+        *["dirname"]=> string(12) "/demo/upload" 
+        *["basename"]=> string(7) "xxx.jpg"
+        *["extension"]=> string(3) "jpg" 
+        *["filename"]=> string(3) "xxx" 
+        * }
+      */
+      if (($GLOBALS["_config"]["CacheTime"] == 0 or time() - @filemtime($CacheFile) < $GLOBALS["_config"]["CacheTime"]) && file_exists($CacheFile)) 
+      {
+        //缓存时间内
+        $Cache_json = json_decode(file_get_contents($CacheFile));
+        if ($Cache_json->webp) {
+          $temp_path = $url_info_file["dirname"] == "/" ?$url_info_file["dirname"] . $url_info_file["filename"] . ".webp" : $url_info_file["dirname"] . "/" . $url_info_file["filename"] . ".webp";
+        } else {
+          $temp_path = $GLOBALS["real_url_p"];
+        }
+        //已缓存 立即导航jsdelivr
+        //如果设置跳转缓存更加高效
+        header('Location: https://cdn.jsdelivr.net/gh/' . $GLOBALS["_config"]["username"] . "/" . $GLOBALS["_config"]["repos"] . $GLOBALS["_config"]["path"] . $temp_path);
+        die;
+      }
+      header('Location: ' . $GLOBALS["_config"]["site"] .$GLOBALS["_config"]["path"]. $GLOBALS["real_url_n"]);
+      fastcgi_finish_request();
+      $if_webp=isset($GLOBALS["extension_webp"][$url_info_file["extension"]]) && $GLOBALS["_config"]["webp"];
+      $Cache_json = array("webp" => $if_webp);
+      if($if_webp) { 
+        $temp_func = "imagecreatefrom" . $GLOBALS["extension_webp"][$url_info_file["extension"]];
+        $temp_path = $url_info_file["dirname"] == "/" ? $url_info_file["dirname"] . $url_info_file["filename"] . ".webp" : $url_info_file["dirname"] . "/" . $url_info_file["filename"] . ".webp";
+        //目录
+        $img_file = fopen('img/' . $url_info_file["basename"], "w+");
+        fwrite($img_file, file_get_contents($GLOBALS["_config"]["site"] . $GLOBALS["_config"]["path"] . $GLOBALS["real_url_p"]));
+        fclose($img_file);
+        //获取到远程图片
+        $temp_img = $temp_func('img/' . $url_info_file["basename"]);
+        imagewebp($temp_img, 'img/' . $url_info_file["filename"] . ".webp");
+        imagedestroy($temp_img);
+        //写出webp图片
+        $file_data = file_get_contents('img/' . $url_info_file["filename"] . ".webp");
+        if (file_exists('img/' . $url_info_file["filename"] . ".webp")) @unlink('img/' . $url_info_file["filename"] . ".webp");
+        if (file_exists('img/' . $url_info_file["basename"])) @unlink('img/' . $url_info_file["basename"]);
+        //清理写出缓存
+      } else { 
+        $temp_path = $GLOBALS["real_url_n"];
+        $file_data = file_get_contents($GLOBALS["_config"]["site"] . $GLOBALS["_config"]["path"] . $GLOBALS["real_url_p"]);
+      }
+      $temp_ret="";
+      $result = files_upload($GLOBALS["_config"]["username"], $GLOBALS["_config"]["token"], $GLOBALS["_config"]["repos"], $temp_path, base64_encode($file_data),$temp_ret);
+      if (!$result) $result = files_updata($GLOBALS["_config"]["username"], $GLOBALS["_config"]["token"], $GLOBALS["_config"]["repos"], $temp_path, base64_encode($file_data), get_sha($GLOBALS["_config"]["username"], $GLOBALS["_config"]["repos"], $temp_path),$temp_ret);
+      //再次出错建议写出日志
+      $Cache_json["result"] = $result;
+      if (!$result){ 
+        file_put_contents("log/".time().".txt","\n上传目录:".$temp_path."\n".$result);
+      }else{
+        file_put_contents($CacheFile, json_encode($Cache_json));
+        //正常写入
+      }
+      //写出记录
+    }
